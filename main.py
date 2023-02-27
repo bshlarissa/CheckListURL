1d7ae004a24b81fe88ca2efcf4b854d6f9605b72
bshlarissa <bslarissa1112@gmail.com>
2023-02-26 21:36:42 -0300

    Automação  para procurar palavras nas URL's finalizada.

--- main.py
+++ main.py
@@ -3,29 +3,48 @@ import pandas as pd
 
 from scripts.checkWordInURL import CheckWordInURL
 
+import warnings
+warnings.filterwarnings("ignore") #Remove Warning desnecessário
+
 class GetDataCheckList:
 
     def __init__(self) -> None:
         self.folder_project=os.getcwd()
+        self.folder_planilhas=f"{self.folder_project}/planilhas"
         self.planilha=self.__get_dados_parametros()
         self.word_list=self.__get_list_word_from_parametros()
         self.url_list=self.__get_list_url_from_parametros()
+        
+        self.__get_result()
 
 
     def __get_dados_parametros(self) -> pd.DataFrame:
-        planilha=pd.read_excel(f"{self.folder_project}/planilhas/parametros.xlsx")
-        return planilha
+        return pd.read_excel(f"{self.folder_planilhas}/parametros.xlsx")
 
 
-    def _find_word_in_url(self,url:str):
-        result=CheckWordInURL(word_list=self.word_list,url=url)._get_word_in_url()
+    def __get_result(self):
+        result=self.__find_words_in_url()
+        print(result)
+        result.to_excel(f"{self.folder_planilhas}/result.xlsx",index=False)
+
+
+    def __find_words_in_url(self) -> pd.DataFrame:
+        result = pd.DataFrame()
+        for url in self.url_list:
+            print(f"\nProcurando palavras na URL: {url}")
+            word_result=CheckWordInURL(word_list=self.word_list,url=url)._get_words_in_url()
+            result=result.append(word_result)
+
+        return result
+
 
 
     def __get_list_word_from_parametros(self) -> list:
-        return self.planilha['palavras_chave'].to_list()
+        return self.planilha['palavras_chave'].dropna().to_list()
+
     
 
     def __get_list_url_from_parametros(self) -> list:
-        self.planilha['urls'].to_list()
+        return self.planilha['urls'].dropna().to_list()
 
-planilha=GetDataCheckList()._find_word_in_url(url="https://www.ofertaesperta.com/")
+GetDataCheckList()
--- scripts/checkWordInURL.py
+++ scripts/checkWordInURL.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 
 from scripts.webDriver import SeleniumWebDriver
 
+import pandas as pd
+
 
 class CheckWordInURL:
     def __init__(self,word_list:list,url:str) -> None:
@@ -10,10 +12,11 @@ class CheckWordInURL:
         self.word_list=word_list
         self.url=url
         self.list_words_in_url=[]
+        self.list_url=[]
 
 
     def __is_word_in_url(self,soup,word:str) -> bool:
-        matched_tags = soup.find_all(lambda tag: len(tag.find_all()) == 0 and word in tag.text)
+        matched_tags = soup.find_all(lambda tag: len(tag.find_all()) == 0 and word in tag.text.lower())
 
         for matched_tag in matched_tags:        
             if matched_tag != "":
@@ -21,7 +24,7 @@ class CheckWordInURL:
             else:
                 return False
             
-    def _get_word_in_url(self) -> list:
+    def _get_words_in_url(self) -> pd.DataFrame:
         self.driver.get(self.url)
 
         #with BeautifulSoup
@@ -29,6 +32,11 @@ class CheckWordInURL:
         soup = BeautifulSoup(html, 'lxml')
 
         for word in self.word_list: 
-            string = word.capitalize()
-            if self.__is_word_in_url(soup,string):
-                print(f"{string} encontrado")
+            if self.__is_word_in_url(soup,word):
+                self.list_words_in_url.append(word)
+                self.list_url.append(self.url)
+
+        return pd.DataFrame(data={
+            "palavras_chave":self.list_words_in_url,
+            "url":self.list_url
+            })
